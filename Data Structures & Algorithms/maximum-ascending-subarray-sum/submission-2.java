aaf770b545da2e3094ce0a996a3f884b7f4629dc
Adithya KrishnaMurthy <adithyaMelige@gmail.com>
2026-08-29 13:08:09 +0530

    Add: maximum-ascending-subarray-sum - submission-2

---
+++ Data Structures & Algorithms/maximum-ascending-subarray-sum/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int sum  = nums[0];
+        int max = sum;
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] > nums[i-1])
+            {
+                sum += nums[i];
+            }else{
+               
+                sum = nums[i];
+               
+            }
+             max = Math.max(max, sum);
+        }
+
+        return max;
+    }
+}
