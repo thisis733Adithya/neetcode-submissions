c41da4cb6ceaed4c8dd4d23c4e09cc61db9f50a9
Adithya KrishnaMurthy <adithyaMelige@gmail.com>
2026-09-11 08:55:54 +0530

    Create Number of Pairs of Interchangeable Rectangles.java

---
+++ Data Structures & Algorithms/Number of Pairs of Interchangeable Rectangles.java
@@ -0,0 +1,60 @@
+/*
+2001. Number of Pairs of Interchangeable Rectangles
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+You are given n rectangles represented by a 0-indexed 2D integer array rectangles, where rectangles[i] = [widthi, heighti] denotes the width and height of the ith rectangle.
+
+Two rectangles i and j (i < j) are considered interchangeable if they have the same width-to-height ratio. More formally, two rectangles are interchangeable if widthi/heighti == widthj/heightj (using decimal division, not integer division).
+
+Return the number of pairs of interchangeable rectangles in rectangles.
+
+ 
+
+Example 1:
+
+Input: rectangles = [[4,8],[3,6],[10,20],[15,30]]
+Output: 6
+Explanation: The following are the interchangeable pairs of rectangles by index (0-indexed):
+- Rectangle 0 with rectangle 1: 4/8 == 3/6.
+- Rectangle 0 with rectangle 2: 4/8 == 10/20.
+- Rectangle 0 with rectangle 3: 4/8 == 15/30.
+- Rectangle 1 with rectangle 2: 3/6 == 10/20.
+- Rectangle 1 with rectangle 3: 3/6 == 15/30.
+- Rectangle 2 with rectangle 3: 10/20 == 15/30.
+Example 2:
+
+Input: rectangles = [[4,5],[7,8]]
+Output: 0
+Explanation: There are no interchangeable pairs of rectangles.
+ 
+
+Constraints:
+
+n == rectangles.length
+1 <= n <= 105
+rectangles[i].length == 2
+1 <= widthi, heighti <= 105
+*/
+
+ public long interchangeableRectangles(int[][] rectangles) {
+        long count = 0;
+        HashMap<Double, Long> map = new HashMap<>();
+        for(int i=0; i<rectangles.length; i++){
+            double div = ((double)rectangles[i][0]/
+                      (double)rectangles[i][1]);
+            map.put(div, map.getOrDefault(div , (long)0 )+1);
+        }
+        for(Map.Entry<Double, Long> set : map.entrySet())
+        {
+            long n = set.getValue();
+
+             count += ((n) * (n-1)) / 2;
+        }
+
+        return  count;
+    }
+
